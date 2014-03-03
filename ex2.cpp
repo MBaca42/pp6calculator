6a5c70e20998ec274fb15ba96fff92cb854fefa2
Matthew Baca <mjb@eprex16.ph.bham.ac.uk>
2014-03-03 15:52:33 +0000

    Put the addition/subtraction/multiply/division into separate functions along with a print function

--- ex2.cpp
+++ ex2.cpp
@@ -1,40 +1,55 @@
 #include <iostream>
+#include <climits>
+
+double addition(double a, double b){
+  return a+b;
+}
+double subtraction(double a, double b){
+  return a-b;
+}
+double multiply(double a, double b){
+  return a*b;
+}
+double division(double a, double b){
+  return a/b;
+}
+double print(double c){
+  std::cout << "Value of c is: " << c << '\n';
+  return 0;
+}
+
 int main()
 {
-double a;
-double b;
-double c;
- int input;
+  double a;
+  double b;
+  double c;
+  int input;
 
- //std::cin >> a >> b;
+  int i = 1;
+  while(i < 10){
+    std::cout << "Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division or 5 to quit: ";
+    std::cin >> input;
 
- //c = a*b;
+    if(!std::cin || input >= 6 || input <= 0){
+      std::cout << "Invalid input!";
+      std::cin.clear();
+      std::cin.ignore(INT_MAX, '\n');
+      continue;
+    }
+    if(input == 5){
+      break;
+    }
 
- //std::cout << c;
+    std::cout << "Enter value for a: ";
+    std::cin >> a;
+    std::cout << "Enter value for b: ";
+    std::cin >>b;
 
- std::cout << "Enter value for a: ";
- std::cin >> a;
- std::cout << "Enter value for b: ";
- std::cin >>b;
- std::cout << "Enter 1 for addition, 2 for subtraction, 3 for multiplication or 4 for division: ";
- std::cin >> input;
- if(input == 1){
-   c = a+b; 
-}
- if(input == 2){
-   c = a-b;
- }
- if(input == 3){
-   c = a*b;
- }
- if(input == 4){
-   c = a/b;
- }
- if(!std::cin){
-   std::cout << "Invalid input!";
-}
+    if(input == 1) print( addition(a,b) );
+    if(input == 2) print( subtraction(a,b) );
+    if(input == 3) print( multiply(a,b) );
+    if(input == 4) print( division(a,b) );
 
- std::cout << "Value of c is: " << c;
-
-return 0;
+  }
+  return 0;
 }
