24ec7dfb81afd18d91c69abadcd60e3a81f5d374
Matthew Baca <mjb@eprex16.ph.bham.ac.uk>
2014-03-24 14:12:44 +0000

    Working so far I think

---
+++ PP6Lib/CMakeFiles/pp6lib.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mjb/mpagspp6/pp6calculator.git/PP6Lib/Day1.cpp" "/home/mjb/mpagspp6/pp6calculator.git/PP6Lib/CMakeFiles/pp6lib.dir/Day1.cpp.o"
+  "/home/mjb/mpagspp6/pp6calculator.git/PP6Lib/Day2.cpp" "/home/mjb/mpagspp6/pp6calculator.git/PP6Lib/CMakeFiles/pp6lib.dir/Day2.cpp.o"
+  "/home/mjb/mpagspp6/pp6calculator.git/PP6Lib/Day3.cpp" "/home/mjb/mpagspp6/pp6calculator.git/PP6Lib/CMakeFiles/pp6lib.dir/Day3.cpp.o"
+  "/home/mjb/mpagspp6/pp6calculator.git/PP6Lib/PP6Math.cpp" "/home/mjb/mpagspp6/pp6calculator.git/PP6Lib/CMakeFiles/pp6lib.dir/PP6Math.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "PP6Lib"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ PP6Lib/CMakeFiles/pp6lib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pp6lib.dir/PP6Math.cpp.o"
+  "CMakeFiles/pp6lib.dir/Day1.cpp.o"
+  "CMakeFiles/pp6lib.dir/Day2.cpp.o"
+  "CMakeFiles/pp6lib.dir/Day3.cpp.o"
+  "libpp6lib.pdb"
+  "libpp6lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pp6lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
